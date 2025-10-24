b6dd344ea70d8129358e1cad6e396a213276c486
pmateus770-pixel <pmateus770@gmail.com>
2025-10-23 21:00:27 -0300

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-from fonte_de_dados import get_live_matches, bet365_link
-from logica import classify_btssignal          # se o arquivo chama “lógica.py”, veja passo 3
-from notificador import enviar
-from estado import was_sent, mark_sent
